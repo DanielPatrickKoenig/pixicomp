020138d1ea578e22c6156d9b2321947561c339ba
Dan Koenig <dkoenig@gongos.com>
2019-06-09 21:51:54 -0400

    Signed-off-by: Dan Koenig <dkoenig@gongos.com>

--- src/src/app/components/timeline.component.ts
+++ src/src/app/components/timeline.component.ts
@@ -1,10 +1,10 @@
 import { Component, Input } from "@angular/core";
+import PixiManager from '../objects/PixiManager';
 @Component({
   selector: 'timeline',
   template: `<div>
     <ul *ngIf="scope.tweens != undefined && scope.tweens != null">
-      <li *ngFor="let t of scope.tweens;">
-        !TWEEN!
+      <li *ngFor="let t of scope.tweens;" [ngStyle]="getTweenLineStyle(t)">
         <tweenline [tween]="t"></tweenline>
       </li>
     </ul>
@@ -12,4 +12,23 @@ import { Component, Input } from "@angular/core";
 })
 export class Timeline {
   @Input() scope: any;
+  getTweenLineStyle(tween) {
+    let highestCount = 0;
+    let pm:PixiManager = new PixiManager();
+    let propBaseHeight = pm.TweenUIValues.baseHeight;
+    let widthFactor = pm.TweenUIValues.baseWidth;
+    let totalDuration = 0;
+    for (let i = 0; i < tween.steps.length; i++) {
+      let step = tween.steps[i];
+      totalDuration+=step.duration;
+      let pCount = 1;
+      for (let p in step.properties) {
+        pCount++;
+      }
+      if (pCount > highestCount){
+        highestCount = pCount;
+      }
+    }
+    return {height: (highestCount * propBaseHeight).toString() + 'px', width: (widthFactor * totalDuration) + 'px', display: 'block'};
+  }
 }
